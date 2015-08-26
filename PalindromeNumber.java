f8381fffc5b4ee03a21f71ee485e28f1bbf7e923
smith <smith@localhost.localdomain>
2015-08-25 23:17:45 -0400

    P-9

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -2,7 +2,7 @@ public class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) {
             return false;
-        } else if(x.length() == 0) {
+        } else if(Integer.toString(x).length() == 1) {
                 return true;
         } else {
             StringBuilder rev = new StringBuilder(Integer.toString(x));

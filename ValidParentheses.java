fd8588c612dc9b748a9ece16f17fc121696df5a6
smith <smith@localhost.localdomain>
2015-09-10 10:37:18 -0400

    P-20. Algo from programcreek.com as stack and hashmaps in java is not frequently used for now.

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,20 +1,22 @@
-public class ValidParentheses {
-	public static void main(String[] args) {
-		String input = "(()))(";
-		System.out.println(isValid(input));
-	}
-	public static boolean isValid(String s) {
-		System.out.println(s);
-
-		char[] left = {'(', '[', '{'};
-		char[] right = {')', ']', '}'};
-		for(int i = 0; i < left.length; i++) {
-			char l = left[i];
-			char r = right[i];
-			if(s.indexOf(r) < s.indexOf(l)) {
-				return false;
-			}
-		}
-		return true;
-	}
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> wrkStk = new Stack<Character>();
+        HashMap<Character, Character> wrkMap = new HashMap<Character, Character>();
+        wrkMap.put('(', ')');
+        wrkMap.put('[', ']');
+        wrkMap.put('{', '}');
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(wrkMap.keySet().contains(s.charAt(i))) {
+                wrkStk.push(s.charAt(i));
+            } else if(wrkMap.values().contains(s.charAt(i))) {
+                if(!wrkStk.empty() && wrkMap.get(wrkStk.peek()) == s.charAt(i)) {
+                    wrkStk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return wrkStk.empty();
+    }
 }

b0bcee97f3afe38c5f41f2f8084e5d1668b308c6
smithislearning <cz776@nyu.edu>
2015-09-11 19:42:20 -0400

    P-24. Used algo form csdn, which is the one that I understand. The one from programcreek is a bit trivial for me now. Got to do more things about linked list.

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -8,17 +8,20 @@
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode wrk = head;
-        if(wrk == null || wrk.next == null) {
-            return wrk;
-        }
-        while(wrk != null && wrk.next != null) {
-            int tmpVal = wrk.next.val;
-            wrk.next.val = wrk.val;
-            wrk.val = tmpVal;
-            wrk = wrk.next.next;
-        }
-        wrk = head;
-        return wrk;
-    }
+        if(head == null || head.next == null) {
+			return head;
+		}
+		ListNode wrk = new ListNode(0);
+		wrk.next = head;
+		ListNode tmp1 = wrk;
+		ListNode tmp2 = wrk.next;
+		while(tmp2 != null && tmp2.next != null) {
+			tmp1.next = tmp2.next;
+			tmp2.next = tmp2.next.next;
+			tmp1.next.next = tmp2;
+			tmp1 = tmp2;
+			tmp2 = tmp2.next;
+		}
+		return wrk.next;
+	}
 }

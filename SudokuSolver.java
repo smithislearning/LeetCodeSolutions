df362bd8dae1d0fb1f14f68f6b1760851017da31
smithislearning <cz776@nyu.edu>
2015-09-30 12:34:09 -0400

    P-37. Problems with iterating and delete part. Come back later.

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -3,53 +3,68 @@ public class Solution {
         if(board.length == 0 || board.length % 3 != 0 || board[0].length %3 != 0) {
             return;
         }
-        HashMap<int[], HashSet> wrkMap = new Hashmap<int[], HashSet>();
-        wrkMap = constructMap(board);
-        while(!wrkMap.isEmpty()) {
-            
-            for()
+        HashMap<int[], ArrayList> crtMap = new HashMap<int[], ArrayList>();
+        crtMap = constructMap(board);
+        while(!crtMap.isEmpty()) {
+            for(int[] tmpPos : crtMap.keySet()) {
+                if(crtMap.get(tmpPos).size() == 1) {
+                    board[tmpPos[0]][tmpPos[1]] = (char) crtMap.get(tmpPos).get(0);
+                    crtMap.remove(tmpPos);
+                    crtMap = deleteUnavailable(crtMap, board);
+                }
+            }
         }
     }
     private HashMap constructMap(char[][] input) {
-        HashMap<int[], HashSet> rstMap = new Hashmap<int[], HashSet>();
-        for(int i = 0; i < board.length) {
-            for(int j = 0; j < board[0].length) {
+        HashMap<int[], ArrayList> rstMap = new HashMap<int[], ArrayList>();
+        for(int i = 0; i < input.length; i++) {
+            for(int j = 0; j < input[0].length; j++) {
                 if(input[i][j] == '.') {
                     int[] tmp = {i, j};
-                    HashSet<Character> available = new HashSet<Character>();
-                    for(int val = 0; val < 9; val++) {
-                        available.add(val);
+                    ArrayList<Character> available = new ArrayList<Character>();
+                    for(int val = 1; val < 10; val++) {
+                        available.add((char)(val + 48));
                     }
-                    available = deleteUnavailable(board, tmp, board.length, board[0].length, available);
                     rstMap.put(tmp, available);
                 }
             }
         }
-        rstMap = deleteUnavailable()
+        rstMap = deleteUnavailable(rstMap, input);
         return rstMap;
     }
-    private HashSet deleteUnavailabe(char[][] crtBrd, int[] crtPos, int hLen, int vLen, HashSet crtSet) {
-        int hRg = crtPos[0] / 3;
-        int vRg = crtPos[1] / 3;
-        for(int h = 0; h < hLen; h++) {
-            if(crtBrd[h][crtPos[1]] != '.') {
-                crtSet.remove(crtBrd[h][crtPos[1]]);
-            }
-        }
-        for(int v = 0; v < vLen; v++) {
-            if(crtBrd[crtPos[0]][v] != '.') {
-                crtSet.remove(crtBrd[crtPos[0]][v]);
-            }
-        }
-        for(int h = 0; h < 3; h++) {
-            for(int v = 0; v < 3; v++) {
-                int hIdx = hRg + h;
-                int vIdx = vRg = v;
-                if(crtBrd[hIdx][vIdx] != '.') {
-                    crtSet.remove(crtBrd[hIdx][vIdx]):
+    private HashMap<int[], ArrayList> deleteUnavailable(HashMap<int[], ArrayList> wrkMap, char[][] crtBrd) {
+        for(int[] wrkPos : wrkMap.keySet()) {
+            ArrayList<Character> crtList = wrkMap.get(wrkPos);
+            if(crtList.size() == 0) {
+                wrkMap.remove(wrkPos);
+            } else {
+                for(int i = 0; i < crtBrd.length; i++) {
+                    char tmp = crtBrd[i][wrkPos[1]];
+                    if(crtList.contains(tmp)) {
+                        crtList.remove(crtList.indexOf(tmp));
+                    }
+                }
+                for(int j = 0; j < crtBrd[0].length; j++) {
+                    char tmp = crtBrd[wrkPos[0]][j];
+                    if(crtList.contains(tmp)) {
+                        crtList.remove(crtList.indexOf(tmp));
+                    }
+                }
+                int hrztl = wrkPos[0] / 3;
+                int vrtcl = wrkPos[1] / 3;
+                for(int i = 0; i < 3; i++) {
+                    for(int j = 0; j < 3; j++) {
+                        char tmp = crtBrd[hrztl + i][vrtcl + j];
+                        if(crtList.contains(tmp)) {
+                            crtList.remove(crtList.indexOf(tmp));
+                        }
+                    }
+                }
+                if(crtList.size() == 0) {
+                    wrkMap.remove(wrkPos);
                 }
             }
         }
-        return crtSet;
+        return wrkMap;
     }
 }

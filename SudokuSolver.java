cdd1e58bebfc1643ff77c38e02da6dc4eb14cba9
smithislearning <cz776@nyu.edu>
2015-09-30 20:07:35 -0400

    P-37. Partially working. Need to put backtrack in it.

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -5,20 +5,30 @@ public class Solution {
         }
         HashMap<int[], ArrayList> crtMap = new HashMap<int[], ArrayList>();
         crtMap = constructMap(board);
+        for(int[] tmp : crtMap.keySet()) {
+            System.out.println(tmp[0] + ", " + tmp[1]);
+            System.out.println(crtMap.get(tmp));
+        }
         while(!crtMap.isEmpty()) {
+            int[]crtPos = {-1, -1};
             for(int[] tmpPos : crtMap.keySet()) {
                 if(crtMap.get(tmpPos).size() == 1) {
                     board[tmpPos[0]][tmpPos[1]] = (char) crtMap.get(tmpPos).get(0);
-                    crtMap.remove(tmpPos);
-                    crtMap = deleteUnavailable(crtMap, board);
+                    crtPos = tmpPos;
+                    break;
                 }
             }
+            if(crtPos[0] == -1 || crtPos[1] == -1) {
+                break;
+            }
+            crtMap.remove(crtPos);
+            crtMap = deleteUnavailable(crtMap, board);
         }
     }
     private HashMap constructMap(char[][] input) {
         HashMap<int[], ArrayList> rstMap = new HashMap<int[], ArrayList>();
-        for(int i = 0; i < input.length; i++) {
-            for(int j = 0; j < input[0].length; j++) {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
                 if(input[i][j] == '.') {
                     int[] tmp = {i, j};
                     ArrayList<Character> available = new ArrayList<Character>();
@@ -35,33 +45,26 @@ public class Solution {
     private HashMap<int[], ArrayList> deleteUnavailable(HashMap<int[], ArrayList> wrkMap, char[][] crtBrd) {
         for(int[] wrkPos : wrkMap.keySet()) {
             ArrayList<Character> crtList = wrkMap.get(wrkPos);
-            if(crtList.size() == 0) {
-                wrkMap.remove(wrkPos);
-            } else {
-                for(int i = 0; i < crtBrd.length; i++) {
-                    char tmp = crtBrd[i][wrkPos[1]];
+            for(int i = 0; i < 9; i++) {
+                char tmp = crtBrd[i][wrkPos[1]];
+                if(crtList.contains(tmp)) {
+                    crtList.remove(crtList.indexOf(tmp));
+                }
+            }
+            for(int j = 0; j < 9; j++) {
+                char tmp = crtBrd[wrkPos[0]][j];
+                if(crtList.contains(tmp)) {
+                    crtList.remove(crtList.indexOf(tmp));
+                }
+            }
+            int hrztl = wrkPos[0] / 3;
+            int vrtcl = wrkPos[1] / 3;
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    char tmp = crtBrd[3*hrztl + i][3*vrtcl + j];
                     if(crtList.contains(tmp)) {
                         crtList.remove(crtList.indexOf(tmp));
                     }
-                }
-                for(int j = 0; j < crtBrd[0].length; j++) {
-                    char tmp = crtBrd[wrkPos[0]][j];
-                    if(crtList.contains(tmp)) {
-                        crtList.remove(crtList.indexOf(tmp));
-                    }
-                }
-                int hrztl = wrkPos[0] / 3;
-                int vrtcl = wrkPos[1] / 3;
-                for(int i = 0; i < 3; i++) {
-                    for(int j = 0; j < 3; j++) {
-                        char tmp = crtBrd[hrztl + i][vrtcl + j];
-                        if(crtList.contains(tmp)) {
-                            crtList.remove(crtList.indexOf(tmp));
-                        }
-                    }
-                }
-                if(crtList.size() == 0) {
-                    wrkMap.remove(wrkPos);
                 }
             }
         }

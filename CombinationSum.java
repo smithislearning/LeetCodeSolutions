e89c8e9c4177f3777a9db45370d5bd407ac7f33b
smithislearning <cz776@nyu.edu>
2015-10-13 15:29:44 -0400

    P-39. Not yet finished.

--- CombinationSum.java
+++ CombinationSum.java
@@ -2,27 +2,26 @@ public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
         List<Integer> tmpRst = new ArrayList<Integer>();
-        if(candidates.length == 0 || target < 0) {
-            return rst;
-        }
         int init = 0;
         int tail = init;
         int sum = candidates[init];
-        tmpRst.add(0, candidates[init]);
-        while(candidates.length > 0) {
+        tmpRst.add(candidates[init]);
+        while(true) {
             while(target - sum >= candidates[tail]) {
                 sum += candidates[tail];
                 tmpRst.add(candidates[tail]);
             }
-            System.out.println(tail);
-            System.out.println(sum);
-            System.out.println(tmpRst);
-            if(sum >= target || tmpRst.size() < 1 || tail >= candidates.length - 1) {
-                int[] wrk = Arrays.copyOfRange(candidates, 1, candidates.length);
-                rst = combinationSum(wrk, target);
-                if(sum == target) {
-                    rst.add(tmpRst);
+            if(sum >= target || tail >= candidates.length - 1) {
+                if(candidates.length > 1) {
+                    int[] wrk = Arrays.copyOfRange(candidates, 1, candidates.length);
+                    rst = combinationSum(wrk, target);
                 }
+                if(sum == target) {
+                    HashSet<List<Integer>> wrkset = new HashSet<List<Integer>>(rst);
+                    wrkset.add(tmpRst);
+                    rst = new ArrayList<List<Integer>>(wrkset);
+                }
+                break;
             } else {
                 sum -= candidates[tail];
                 tmpRst.remove(tmpRst.size() - 1);

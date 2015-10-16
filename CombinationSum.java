4d1e08c807a00ef14df30fde02a6f5852bca89c8
smithislearning <cz776@nyu.edu>
2015-10-16 16:22:02 -0400

    P-39. Algo itself works. But to slow. Still working on it.

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,33 +1,50 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        List<List<Integer>> tmp = new ArrayList<List<Integer>>();
         List<Integer> tmpRst = new ArrayList<Integer>();
-        int init = 0;
-        int tail = init;
-        int sum = candidates[init];
-        tmpRst.add(candidates[init]);
-        while(true) {
-            while(target - sum >= candidates[tail]) {
-                sum += candidates[tail];
-                tmpRst.add(candidates[tail]);
+        if(candidates.length == 1) {
+            int sum = 0;
+            while(sum < target) {
+                sum += candidates[0];
+                tmpRst.add(candidates[0]);
             }
-            if(sum >= target || tail >= candidates.length - 1) {
-                if(candidates.length > 1) {
-                    int[] wrk = Arrays.copyOfRange(candidates, 1, candidates.length);
-                    rst = combinationSum(wrk, target);
+            if(sum == target) {
+                rst.add(tmpRst);
+            }
+            return rst;
+        }
+        int[] tmpwrk = Arrays.copyOfRange(candidates, 0, candidates.length - 1);
+        tmp = combinationSum(tmpwrk, target);
+        if(!tmp.isEmpty()) {
+            rst.addAll(tmp);
+        }
+        int trgt = target;
+        int cnt = 0;
+        while(trgt > candidates[candidates.length - 1]) {
+            cnt++;
+            trgt -= candidates[candidates.length - 1];
+            tmp = combinationSum(tmpwrk, trgt);
+            if(!tmp.isEmpty()) {
+                tmpRst.clear();
+                for(int i = 0; i < cnt; i++) {
+                    tmpRst.add(candidates[candidates.length - 1]);
                 }
-                if(sum == target) {
-                    HashSet<List<Integer>> wrkset = new HashSet<List<Integer>>(rst);
-                    wrkset.add(tmpRst);
-                    rst = new ArrayList<List<Integer>>(wrkset);
+                for(int i = 0; i < tmp.size(); i++) {
+                    tmp.get(i).addAll(tmpRst);
                 }
-                break;
-            } else {
-                sum -= candidates[tail];
-                tmpRst.remove(tmpRst.size() - 1);
-                tail++;
+                rst.addAll(tmp);
             }
         }
+        if(candidates.length > 1) {
+            int[] tmpwrk2 = Arrays.copyOfRange(candidates, 1, candidates.length);
+            tmp = combinationSum(tmpwrk2, target);
+            if(!tmp.isEmpty()) {
+                rst.addAll(tmp);
+            }
+        }
+        HashSet<List<Integer>> rstHsh = new HashSet<List<Integer>>(rst);
+        rst = new ArrayList<List<Integer>>(rstHsh);
         return rst;
     }
 }

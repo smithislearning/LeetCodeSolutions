92c32e0c1dd3cc192a77450fe382e1767bb3547d
root <root@localhost.localdomain>
2016-09-06 17:04:11 -0700

    some changes

--- AddBinary.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-public class Solution {
-    public String addBinary(String a, String b) {
-        BigInteger aBig = new BigInteger(a, 2);
-        BigInteger bBig = new BigInteger(b, 2);
-        BigInteger rst = aBig.add(bBig);
-        return rst.toString(2);
-    }
-}
--- MultiplyStrings.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-public class Solution {
-    public String multiply(String num1, String num2) {
-        BigInteger bigNum1 = new BigInteger(num1);
-        BigInteger bigNum2 = new BigInteger(num2);
-        BigInteger bigRst = bigNum1.multiply(bigNum2);
-        return bigRst.toString();
-    }
-}

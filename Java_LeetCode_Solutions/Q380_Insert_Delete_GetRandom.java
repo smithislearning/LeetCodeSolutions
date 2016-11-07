31b03e7d1fc8728c2df06cdcd43208bf423ebbeb
smith <smithislearning@gmail.com>
2016-11-07 14:03:30 -0800

    Q380: Insert Delete GetRandom

--- Java_LeetCode_Solutions/Q380_Insert_Delete_GetRandom.java
+++ Java_LeetCode_Solutions/Q380_Insert_Delete_GetRandom.java
@@ -1,57 +1,113 @@
 public class RandomizedSet {
+
     private HashMap<Integer, Integer> store;
-    private HashMap<Integer, Integer> idxSt;
+
+    private HashMap<Integer, Integer> idx;
+
+
 
     /** Initialize your data structure here. */
+
     public RandomizedSet() {
+
         store = new HashMap<Integer, Integer>();
-        idxSt = new HashMap<Integer, Integer>();
+
+        idx = new HashMap<Integer, Integer>();
+
     }
+
     
+
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+
     public boolean insert(int val) {
+
         if(store.containsKey(val)) {
+
             return false;
+
         }
+
         store.put(val, store.size());
-        idxSt.put(idxSt.size(), val);
+
+        idx.put(idx.size(), val);
+
         return true;
+
     }
+
     
+
     /** Removes a value from the set. Returns true if the set contained the specified element. */
+
     public boolean remove(int val) {
+
         if(!store.containsKey(val)) {
+
             return false;
+
         }
+
         int tmp = store.get(val);
+
         store.remove(val);
-        idxSt.remove(tmp);
-        if(tmp == store.size() || store.size() == 0) {
+
+        idx.remove(tmp);
+
+        if(store.isEmpty() || tmp == store.size()) {
+
             return true;
+
         }
-        int tmpKey = idxSt.get(idxSt.size());
-        store.replace(tmpKey, store.get(tmpKey), tmp);
-        idxSt.remove(idxSt.size());
-        idxSt.put(store.get(tmpKey), tmpKey);
+
+        int tmp1 = idx.get(idx.size());
+
+        store.replace(tmp1, store.get(tmp1), tmp);
+
+        idx.remove(idx.size());
+
+        idx.put(store.get(tmp1), tmp1);
+
         return true;
+
     }
+
     
+
     /** Get a random element from the set. */
+
     public int getRandom() {
-        if(store.size() == 0) {
+
+        if(store.isEmpty()) {
+
             return 0;
+
         }
+
         if(store.size() == 1) {
-            return idxSt.get(0);
+
+            return idx.get(0);
+
         }
-        return idxSt.get(new Random().nextInt(store.size()));
+
+        return idx.get(new Random().nextInt(store.size()));
+
     }
+
 }
 
+
+
 /**
+
  * Your RandomizedSet object will be instantiated and called as such:
+
  * RandomizedSet obj = new RandomizedSet();
+
  * boolean param_1 = obj.insert(val);
+
  * boolean param_2 = obj.remove(val);
+
  * int param_3 = obj.getRandom();
+
  */
